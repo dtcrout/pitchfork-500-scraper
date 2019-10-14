6a994e9f9ebd52f0f58f3cd57aaa71eca293baf8
Darshan Crout <dtcrout@gmail.com>
2019-10-14 13:57:48 -0400

    Add tracks to json

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"log"
 	"net/http"
 	"strings"
 )
+
+type Track struct {
+	Artist string
+	Song   string
+}
 
 func ParseWiki(url string) {
 	resp, err := http.Get(url)
@@ -26,11 +32,16 @@ func ParseWiki(url string) {
 	// Find all songs on page and parse string into artist and song
 	doc.Find(".div-col").Each(func(_ int, s *goquery.Selection) {
 		s.Find("li").Each(func(_ int, t *goquery.Selection) {
-			text := t.Text()
-			s := strings.Split(text, " –")
-			artist := s[0]
-			song := s[1]
-			fmt.Println(artist, song)
+			text := strings.Split(t.Text(), " –")
+
+			artist := text[0]
+			song := strings.Trim(text[1], " \"")
+
+			// Create track
+			track_obj := Track{Artist: artist, Song: song}
+			track, _ := json.Marshal(track_obj)
+
+			fmt.Println(string(track))
 		})
 	})
 }
